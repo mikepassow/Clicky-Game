bc8acce16439fbeecebee40d083ea4690a82a129
Michael Passow <passowmichael@yahoo.com>
2019-10-16 18:21:54 -0400

    added alerts

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,7 +5,6 @@ import CharCard from "./components/CharCard";
 import Score from "./components/Score";
 import Alert from "./components/Alert";
 import NavBar from "./components/NavBar";
-import BottomNavMDC from "./components/BottomNavMDC";
 import characters from "./characters.json";
 
 class App extends Component {
